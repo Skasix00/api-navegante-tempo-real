3a407f795c58efb922f4c4b24a1bb8047dc7ea02
Bruno Castelo <brunoalexcastelo@gmail.com>
2026-05-04 11:50:19 +0100

    refactor: remove sync-alerts and sync-vehicles modules to streamline project structure
    - Deleted the `sync-alerts` and `sync-vehicles` modules along with their associated files, including Dockerfiles, package.json, and TypeScript configurations, to simplify the project and reduce maintenance overhead.
    - Updated the main `package.json` to remove references to the deleted modules, enhancing clarity and focus on active components.

--- shared/services/src/index.ts
+++ shared/services/src/index.ts
@@ -1,6 +1,4 @@
 export * from './IXAPI.js';
 export * from './NETWORKDB.js';
-export * from './PCGIAPI.js';
-export * from './PCGIDB.js';
 export * from './redis.service.js';
 export * from './SERVERDB.js';
